994faebe4c4888b7992d5a7e733989c7aa29245f
ayan471 <pakhiraayan@gmail.com>
2023-07-05 11:35:38 +0530

    fixed some minor bugs

--- app.js
+++ app.js
@@ -40,10 +40,4 @@ app.use("/api/v1", other);
 
 export default app;
 
-app.get("/", (req, res) =>
-  res.send(
-    `<h1> Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
-  )
-);
-
 app.use(ErrorMiddleware);
